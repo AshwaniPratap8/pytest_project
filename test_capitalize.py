7025b45f2cb931e72d3a152121cfe1c240989f3f
yogesh73 <44692270+yogesh73@users.noreply.github.com>
2021-08-13 03:00:06 +0530

    Update test_capitalize.py

--- test_capitalize.py
+++ test_capitalize.py
@@ -5,6 +5,9 @@ def capital_case(x):
 
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
+
+def test_capital_case_Negative():
+    assert capital_case('semaphore') == 'Semapho'
 	
 def test_capital_case2():
     assert capital_case('phore') == 'Phore'
